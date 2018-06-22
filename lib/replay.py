bf018a92cd08561babc3fc70e82c687b724ca082
zjc-enigma <enigma@pku.edu.cn>
2018-06-22 23:19:33 +0800

    added replay sgf

---
+++ lib/replay.py
@@ -0,0 +1,36 @@
+import time
+from board import Board
+from sgfmill import sgf
+import pdb
+
+
+class Replay:
+
+    def __init__(self, sgf_file):
+        self._load_game(sgf_file)
+        self.board = Board()
+
+    def _load_game(self, sgf_file):
+        with open(sgf_file, "rb") as rfd:
+            game = sgf.Sgf_game.from_bytes(rfd.read())
+
+        self.size = game.get_size()
+        self.winner = game.get_winner()
+        root_node = game.get_root()
+        self.black_name = root_node.get("PB")
+        self.white_name = root_node.get("PW")
+        self.game_steps = game.get_main_sequence()
+
+    def auto_play(self, delay=0.5):
+        for step in self.game_steps:
+            _, coordinate = step.get_move()
+            if coordinate is None:
+                continue
+            self.board.move_one_step(coordinate)
+            time.sleep(delay)
+
+
+if __name__ == "__main__":
+
+    r = Replay("../data/2t9d-围棋gokifu棋谱-20171025-柯洁九段-李世石九段.sgf")
+    r.auto_play()
